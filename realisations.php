8f32e494e719da3f45a7efba4e4e11f4c7f35c98
Robin <barkasrobin@gmail.com>
2016-09-19 11:11:59 +0200

    Réparation de TWIG + modif gitignore

--- realisations.php
+++ realisations.php
@@ -6,7 +6,7 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
 	'debug' => 'true',
-	'cache' => 'cache',
+	'cache' => 'cache/Twig',
 ));
 
 $data['tags'] = array(
